5316216e57f047ceb4542f3e2128f8c476fb3864
Bfarkiani <b.farkiani@wustl.edu>
2024-11-18 22:10:09 -0600

    typo

--- Controller/run.sh
+++ Controller/run.sh
@@ -6,5 +6,5 @@ docker load -i $FILE
 VERSION=$(echo $FILE | sed 's/^.*_\(.*\)\.tar/\1/')
 
 echo "running Controller..."
-docker run -p 18000:18000 -p 19000:19000 --name Controller -t -v "$PWD/controller.crt":/controller/controller.crt -v "$PWD/controller.key":/controller/controller.key envoy_controller:$VERSION -dbHost=<DB IP HERE> -updateTime=5s
+docker run -p 18000:18000 -p 19000:19000 --name Controller -t -v "$PWD/controller.crt":/controller/controller.crt -v "$PWD/controller.key":/controller/controller.key envoy_controller:$VERSION -dbHost=<DB_ADDRESS> -updateTime=5s
 echo "Done"
